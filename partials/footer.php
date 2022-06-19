8185de0fb74bfecd769f7a746797a3bdebd1516b
Gokhan Ucar <gokhanucar10@gmail.com>
2022-06-19 20:44:39 +0300

    project init

---
+++ partials/footer.php
@@ -0,0 +1,16 @@
+<!-- footer content -->
+<footer>
+    <div class="pull-right">
+        Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+    </div>
+    <div class="clearfix"></div>
+</footer>
+<!-- /footer content -->
+</div>
+</div>
+
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/fastclick.js"></script>
+<script src="assets/js/nprogress.js"></script>
+<script src="assets/js/custom.min.js"></script>
---
+++ partials/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Fitness Tracking Panel</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/nprogress.css">
+    <link rel="stylesheet" href="assets/css/custom.min.css">
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
